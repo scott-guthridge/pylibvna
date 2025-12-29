18f62c6577250bbf247e747a65fb5bb220500fc0
Scott Guthridge <scott_guthridge@rompromity.net>
2025-12-29 14:58:16 -0800

    If only libvna is imported, lazily import the modules
    Before, just:
        import libvna
    
    didn't do anything useful.  You had to either say:
        from libvna import *
    
    or explicitly import each module needed:
        import libvna.cal
        import libvna.conv
        import libvna.data
    
    Now the following works:
        import libvna
    
        calset = libvna.cal.Calset()
    
    importing the "cal" module only if it's accessed.

--- libvna/__init__.py
+++ libvna/__init__.py
@@ -1,1 +1,12 @@
+import importlib
+
 __all__ = [ "cal", "conv", "data" ]
+
+# If libvna only is imported, lazily load the submodules as they're
+# accessed.
+def __getattr__(name):
+    if name in __all__:
+        module = importlib.import_module(f"{__name__}.{name}")
+        globals()[name] = module
+        return module
+    raise AttributeError(f"module {__name__} has no attribute {name}")
